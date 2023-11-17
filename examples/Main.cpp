e0cd8776031022b7d1ff81ad5ffab52c1011603a
thioblxb <thioblxb@proton.me>
2023-11-17 12:34:03 +0100

    Creating a GLFW window

--- examples/Main.cpp
+++ examples/Main.cpp
@@ -1,4 +1,55 @@
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <string>
+
+// Constants
+const unsigned int WINDOW_WIDTH  {800};
+const unsigned int WINDOW_HEIGHT {600};
+const std::string  WINDOW_TITLE  {"Kedarium Engine"};
+
 int main()
 {
+  // Initializing GLFW
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // Creating the GLFW Window
+  GLFWwindow* window = glfwCreateWindow(
+    WINDOW_WIDTH,
+    WINDOW_HEIGHT,
+    WINDOW_TITLE.c_str(),
+    NULL,
+    NULL
+  );
+
+  // Validating the Window
+  if (window == NULL)
+  {
+    std::cerr << "Failed to create a GLFW window!\n";
+    glfwTerminate();
+    return 1;
+  }
+  glfwMakeContextCurrent(window);
+
+  // Clear Color
+  GLclampf red   = 0.f;
+  GLclampf green = 0.f;
+  GLclampf blue  = 0.f;
+  GLclampf alpha = 1.f;
+  glClearColor(red, green, blue, alpha);
+
+  // Main Loop
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
   return 0;
 }
