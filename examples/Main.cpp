d30b81e1ba3f87c9ade6726237a03869203f3033
thioblxb <thioblxb@proton.me>
2023-11-17 16:55:18 +0100

    Drawing a square

--- examples/Main.cpp
+++ examples/Main.cpp
@@ -10,6 +10,38 @@ const unsigned int WINDOW_WIDTH  {800};
 const unsigned int WINDOW_HEIGHT {600};
 const std::string  WINDOW_TITLE  {"Kedarium Engine"};
 
+// Shader Sources
+const char* vertexShaderSource =
+  "#version 330 core\n"
+  "layout (location = 0) in vec3 aPos;\n"
+  "layout (location = 1) in vec3 aCol;\n"
+  "out vec3 vertCol;\n"
+  "void main()\n"
+  "{\n"
+  "  gl_Position = vec4(aPos, 1.f);\n"
+  "  vertCol = aCol;\n"
+  "}\n";
+const char* fragmentShaderSource =
+  "#version 330 core\n"
+  "in vec3 vertCol;\n"
+  "out vec4 FragColor;\n"
+  "void main()\n"
+  "{\n"
+  "  FragColor = vec4(vertCol, 1.f);\n"
+  "}\n";
+
+// Vertices and Indices
+GLfloat vertices[] = {
+  -0.5f, -0.5f, 0.f, 1.f, 1.f, 1.f,
+   0.5f, -0.5f, 0.f, 1.f, 1.f, 1.f,
+  -0.5f,  0.5f, 0.f, 1.f, 1.f, 1.f,
+   0.5f,  0.5f, 0.f, 1.f, 1.f, 1.f,
+};
+GLuint indices[] = {
+  1, 0, 3,
+  2, 3, 0,
+};
+
 int main()
 {
   // Initializing GLFW
@@ -36,6 +68,16 @@ int main()
   }
   glfwMakeContextCurrent(window);
 
+  // Initializing GLEW
+  GLenum err = glewInit();
+  if (GLEW_OK != err)
+  {
+    std::cerr << "Failed to initialize GLEW!\n";
+    std::cerr << "Error: " << glewGetErrorString(err) << '\n';
+    glfwTerminate();
+    return 1;
+  }
+
   // Clear Color
   GLclampf red   = 0.f;
   GLclampf green = 0.f;
@@ -43,18 +85,99 @@ int main()
   GLclampf alpha = 1.f;
   glClearColor(red, green, blue, alpha);
 
+  // Engine and Version Info
   kdr::Core::printEngineInfo();
   std::cout << '\n';
   kdr::Core::printVersionInfo();
+
+  // Vertex and Fragment Shaders
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+
+  glCompileShader(vertexShader);
+  glCompileShader(fragmentShader);
+
+  // Validating the Shaders
+  int success {0};
+  char infoLog[512];
+
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cerr << "Failed to compile the vertex shader!\n";
+    std::cerr << "Error: " << infoLog << '\n';
+  }
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    std::cerr << "Failed to compile the fragment shader!\n";
+    std::cerr << "Error: " << infoLog << '\n';
+  }
+
+  // Shader Program
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success)
+  {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cerr << "Failed to compile the link the shader program!\n";
+    std::cerr << "Error: " << infoLog << '\n';
+  }
+
+  // Deleting the Shaders
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  // VAO, VBO, and EBO
+  GLuint VAO;
+  GLuint VBO;
+  GLuint EBO;
+
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+  glEnableVertexAttribArray(1);
+
+  glBindVertexArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
   // Main Loop
   while (!glfwWindowShouldClose(window))
   {
     glfwPollEvents();
     glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, sizeof(vertices) / sizeof(GLuint), GL_UNSIGNED_INT, NULL);
     glfwSwapBuffers(window);
   }
 
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
+  glDeleteProgram(shaderProgram);
   glfwDestroyWindow(window);
   glfwTerminate();
   return 0;
